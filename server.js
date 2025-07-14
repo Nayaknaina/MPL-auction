9075963a81276f3ca4b607cd48d0fc91ef5f58a6
Nayaknaina <nainanayak288@.com>
2025-07-14 11:57:21 +0530

    Initial commit for MPL Auction

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+// Serve index.html for different dashboard paths
+app.get('/public/:id', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+app.get('/team/:id', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+app.get('/developer/:id', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+  socket.on('placeBid', ({ playerId, bid, teamId }) => { io.emit('bidUpdate', { playerId, bid, teamId }); });
+  socket.on('nextPlayer', () => { io.emit('nextPlayer'); });
+  socket.on('disconnect', () => { console.log('Client disconnected'); });
+  socket.on('ping', (callback) => {
+    console.log("client ping")
+    callback();
+  });
+});
+
+server.listen(3000, () => { console.log('Server running on port 3000'); });
